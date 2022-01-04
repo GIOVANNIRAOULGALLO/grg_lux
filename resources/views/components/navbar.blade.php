8744271827fe3f1a0e1bf846f96a2557145228d5
Giovanni Raoul Gallo <giovanniraoulgallo@gmail.com>
2022-01-04 22:54:38 +0100

    Little bit style

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-grg">
   <div class="container-fluid">
-    <a class="navbar-brand" href="{{route('homepage')}}"><h2>GRG</h2></a>
+    <a class="navbar-brand" href="{{route('homepage')}}">GRG</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -13,6 +13,7 @@
           <li class="nav-item">
             <a class="nav-link" href="{{ route('register') }}">Register</a>
           </li>
+          
         @endguest
         @auth
           <li class="nav-item dropdown">
@@ -32,6 +33,12 @@
             </ul>
           </li>
         @endauth
+        <li class="nav-item">
+          <form class="d-flex" action="{{route('product.search')}}" method="GET">
+            <input class="form-control me-2" type="text" name="q">
+            <button class="btn btn-outline-success" type="submit" >Search</button>
+          </form>
+        </li>
       </ul>
     </div>
   </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,28 @@
 <x-layout>
     <x-slot name="title">Homepage</x-slot>
     <section class="container">
-        <div class="row justify-content-center text-center">
+        <div class="row justify-content-center text-center my-2">
             <div class="col-12">
-                <h1>WELCOME</h1>
+                <h1>LUXURY BECOME TRUE</h1>
             </div>
         </div>
         <div class="row justify-content-center text-center">
             <div class="col-12 col-md-6">
                 <h3><a href="{{route('product.create')}}">CREATE</a></h3>
-                <form class="d-flex" action="{{route('product.search')}}" method="GET">
-                    <input class="form-control me-2" type="text" name="q">
-                    <button class="btn btn-outline-success" type="submit" >Search</button>
-                  </form>
+               
             </div>
         </div>
         <div class="row justify-content-center text-center">
             <div class="col-12  d-flex flex-row flex-grow-5 flex-wrap justify-content-evenly">
                 @foreach ($products as $product)
                 <div class="card mx-3 my-3" style="width: 220px">
-                    <img src="https://picsum.photos/200" class="card-img-top mx-auto img-fluid" alt="{{$product->name}}">
+                    <a href="{{route('product.show',compact('product'))}}"><img src="https://picsum.photos/200" class="card-img-top mx-auto img-fluid" alt="{{$product->name}}"></a>
                     <div class="card-body">
                       <p class="card-text">{{$product->name}}</p>
                       <p class="text-secondary">{{$product->brand->name ?? 'NULL'}}</p>
                       <p class="card-text"> € {{$product->price}}</p>
                     </div>
-                    <button><a href="{{route('product.show',compact('product'))}}">Details</a></button>
+                    <a href="#" class="btn-grg my-2">Add to cart</a>
                   </div>
                 @endforeach
             </div>
