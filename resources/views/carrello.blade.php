8b54ad1b0d143526c31fee98ebb1d25bc9651b9b
Giovanni Raoul Gallo <giovanniraoulgallo@gmail.com>
2022-08-13 16:10:21 +0200

    modificato vista carrello , da rivedere

--- resources/views/carrello.blade.php
+++ resources/views/carrello.blade.php
@@ -19,35 +19,30 @@
             </div>
         </div>
         @if(\App\Models\Product::where('buy',1)->count()>0)
-        <div class="row  justify-content-center  my-2">
+        <div class="row  justify-content-center align-items-center  my-2">
             @php($count=0)
             
-            <div class="col-12 col-md-6 d-flex flex-row flex-wrap justify-content-end">
+            <div class="col-12 col-md-6 d-flex flex-column justify-content-center">
                 
                 @foreach ($products as $product)
-                    <div class="card mb-3 mx-3 text-center" style="height: 140px;width:200px">
-                        <div class="row  justify-content-center flex-wrap flex-grow-2">
-                            <div class="col-md-4">
-                                <a href="{{route('product.show',compact('product'))}}"><img src="https://picsum.photos/50" class="img-fluid my-2" alt="{{$product->name}}"></a>
-                                <form action="{{route('removeToCart',compact('product'))}}" method="post">
-                                    @csrf
-                                    @method('post')
-                                    <button type="submit" class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
-                                </form>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title text-secondary">{{$product->brand->name}}</h5>
-                                    <p class="card-text">{{$product->name}}</p>
-                                    <p class="card-text">{{$product->price}}$</p>
+                   
+                                <div class="card mb-3 mx-3 text-center d-flex flex-row justify-content-center align-items-center" style="height:200px;width:50%">
+                                    <a href="{{route('product.show',compact('product'))}}"><img src="https://picsum.photos/50" class="img-fluid my-2 mx-3" alt="{{$product->name}}"></a>
+                                    <div class="card-body my-auto">
+                                        <h5 class="card-title text-secondary">{{$product->brand->name}}</h5>
+                                        <p class="card-text">{{$product->name}}</p>
+                                        <p class="card-text">{{$product->price}}$</p>
+                                    </div>
+                                    <form action="{{route('removeToCart',compact('product'))}}" method="post">
+                                        @csrf
+                                        @method('post')
+                                        <button type="submit" class="btn btn-danger mx-5"><i class="fa-solid fa-trash"></i></button>
+                                    </form>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
                     @php($count+=$product->price)
                 @endforeach
             </div>
-            <div class="col-12 col-md-6 text-start">
+            <div class="col-12 col-md-6 text-start d-flex align-items-center">
                 <h3>Totale: {{$count}} $</h3>
             </div>
         </div>
